728b031adec7162132b8db127f24e08bd2aba040
Yunita Yani <yunita278yani@gmail.com>
2022-01-04 16:13:48 +0700

    up

--- app/src/main/java/com/example/uiuxaplikasimobile/Login.java
+++ app/src/main/java/com/example/uiuxaplikasimobile/Login.java
@@ -2,7 +2,11 @@ package com.example.uiuxaplikasimobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
+    }
+
+    public void tampil(View view) {
+        Intent intent2 = new Intent(Login.this, Dasboard.class);
+        startActivity(intent2);
     }
 }
--- app/src/main/java/com/example/uiuxaplikasimobile/Register.java
+++ app/src/main/java/com/example/uiuxaplikasimobile/Register.java
@@ -2,7 +2,9 @@ package com.example.uiuxaplikasimobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(Register.this, Login.class);
+        startActivity(intent);
     }
 }
